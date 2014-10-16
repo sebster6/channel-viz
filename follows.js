643bbc8049f8f89b3688907e428b61ebb5b67070
sebster6 <github@skraatz.de>
2014-10-16 18:15:04 +0200

    Update follows.js

--- follows.js
+++ follows.js
@@ -88,7 +88,6 @@
 					 if(duration == '1week') diff = 604800000;
 					 if(duration == '1month') diff = 2628000000;
 					 if(duration == '90days') diff = 7884000000;
-					 if(duration == '1year') diff = 31536000000;
 					then.setTime(now.getTime() - diff);
 					if(updated.getTime() > then.getTime()) {
 						if(datastreamIds && datastreamIds != '' && datastreamIds.indexOf(datastream.id) >= 0) {
