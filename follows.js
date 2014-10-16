c83203c6fea59819aac0049c473ca47648dbbcfd
sebster6 <github@skraatz.de>
2014-10-16 18:23:00 +0200

    Update follows.js

--- follows.js
+++ follows.js
@@ -149,9 +149,8 @@
 										width: 600,
 										height: 400,
 										renderer: 'line',
-										//min: parseFloat(datastream.min_value) - .25*(parseFloat(datastream.max_value) - parseFloat(datastream.min_value)),
-										min: 15,
-										max: parseFloat(datastream.max_value) + .25*(parseFloat(datastream.max_value) - parseFloat(datastream.min_value)),
+										min: parseFloat(datastream.min_value) - .1*(parseFloat(datastream.max_value) - parseFloat(datastream.min_value)),
+										max: parseFloat(datastream.max_value) + .1*(parseFloat(datastream.max_value) - parseFloat(datastream.min_value)),
 										padding: {
 											top: 0.02,
 											right: 0.02,
